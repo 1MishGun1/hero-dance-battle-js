361faf24af28563ca596bfdc828004e5c9fd9c76
1MishGun1 <mikhail.kudrov@gmail.com>
2024-01-14 23:23:38 +0300

    update classes

--- classes.js
+++ classes.js
@@ -26,12 +26,46 @@ class Knight extends Hero {
     this.energy = energy;
   }
 
+  // Chiting
+  dexterityUpKnight(hero) {
+    if (this.energy) {
+      if (this.energy > gameParameters.MIN_STAT) {
+        let dexterityRes = (this.level * this.energy) / 30;
+
+        if (hero.stats.agi + dexterityRes < gameParameters.MAX_STAT) {
+          hero.stats.agi += dexterityRes;
+          console.log(
+            this.name +
+              ' увеличивает ловкость ' +
+              hero.name +
+              ' на ' +
+              dexterityRes +
+              ' единиц.'
+          );
+        } else {
+          hero.stats.agi = gameParameters.MAX_STAT;
+        }
+
+        let dexterityLowerKnight = (dexterityRes * 10) / this.level;
+        if (this.energy - dexterityLowerKnight > gameParameters.MIN_STAT) {
+          this.energy -= dexterityLowerKnight;
+        } else {
+          this.energy = gameParameters.MIN_STAT;
+        }
+
+        displayPlayerHero(playerHero);
+      } else {
+        alert('Мало энергии, мало каши ел');
+      }
+    }
+  }
+
   displayHero() {
     super.displayHero();
     console.log(`Energy: ${this.energy}`);
 
-    if (this.isHorseTango === "true") {
-      console.log("This hero can tango on a horse");
+    if (this.isHorseTango === 'true') {
+      console.log('This hero can tango on a horse');
     }
   }
 }
@@ -43,12 +77,33 @@ class Mage extends Hero {
     this.mana = mana;
   }
 
+  // Chiting
+  hpUpdateMage(hero) {
+    if (this.mana) {
+      if (this.mana > gameParameters.MIN_STAT) {
+        let hpResult = this.level * 15;
+        hero.healthPoints += hpResult;
+        console.log(
+          this.name +
+            ' увеличивает танец ' +
+            hero.name +
+            ' на ' +
+            hpResult +
+            ' очков.'
+        );
+        this.mana -= hpResult * (15 / this.level) - this.level;
+      } else {
+        alert('Мало каши ел :(');
+      }
+    }
+  }
+
   displayHero() {
     super.displayHero();
     console.log(`Mana: ${this.mana}`);
 
-    if (this.hasTectonicPotion === "true") {
-      console.log("There is a potion");
+    if (this.hasTectonicPotion === 'true') {
+      console.log('There is a potion');
     }
   }
 }
